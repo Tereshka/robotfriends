bd8e19a1302c501ebd5c758bee9abb60d31547e7
Елена Агеева <tereshka@softinfo.medic>
2018-06-20 13:06:18 +0900

    small changes

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{overflowY: 'scroll', border: '1px solid black', height: '80vh'}}>
+		<div style={{overflowY: 'scroll', 
+					border: '1px solid black', 
+					height: '80vh'}}>
 			{props.children}
 		</div>
 		);
