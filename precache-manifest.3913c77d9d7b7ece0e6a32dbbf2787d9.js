5cc93b5913c7786f584585e214af7a7c3868a58c
Tereshka <tereshka07@gmail.com>
2019-07-25 13:15:03 +0900

    Updates

---
+++ precache-manifest.3913c77d9d7b7ece0e6a32dbbf2787d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20778d36afad35eae6f3cc0b0aa88de8",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d522ff2da4373467e1db",
+    "url": "/robotfriends/static/css/main.87fc6fdd.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robotfriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "d522ff2da4373467e1db",
+    "url": "/robotfriends/static/js/main.baede67e.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
